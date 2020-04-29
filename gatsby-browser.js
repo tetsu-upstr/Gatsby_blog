d4d223e33e62393ce36f49cb075e96e76f3526ea
tetsu-upstr <bodomu@gmail.com>
2020-04-29 10:53:14 +0900

    ハイライトテーマ変更

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,4 +3,4 @@ import "typeface-montserrat"
 import "typeface-merriweather"
 
 // コードブロックのハイライトテーマ(prism.js)
-import "prismjs/themes/prism-tomorrownight.css"
+import "prismjs/themes/prism-tomorrow.css"
