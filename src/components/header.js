b30b43e25cfbb8f062d4aa11fd2b5ef0460593a0
tetsu-upstr <bodomu@gmail.com>
2020-05-03 10:28:40 +0900

    works.js

--- src/components/header.js
+++ src/components/header.js
@@ -9,8 +9,7 @@ const Header = () =>{
             <nav>
                 <ul>
                     <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/blogindex">Blog</Link></li>
+                    <li><Link to="/works">About</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </nav>
---
+++ src/pages/works.js
@@ -0,0 +1,36 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const WorksPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout location={location} title={siteTitle}>
+
+        こちらでは、作成した成果物を公開しています。
+
+      <h2>作成したWebサイト・アプリケーション</h2>
+
+      <h3>2.鮮度管理アプリ</h3>
+      賞味期限を管理するためのwebアプリケーションです。
+
+      「CRUD」の練習で作成。
+
+      <a href="https://upstr.me/freshness/login.php">KEEP FRESH</a>
+      <h4>使用技術</h4>
+      <ul>
+        <li>PHP</li>
+        <li>MySQL</li>
+      </ul>
+      <h4>実装機能</h4>
+      <ul>
+        <li>商品の登録、編集、削除（PHP）</li>
+        <li>ログイン機能（PHP）</li>
+        <li>商品検索機能（MySQL）</li>
+      </ul>
+
+    </Layout>
+  )
+}
+
+export default WorksPage
